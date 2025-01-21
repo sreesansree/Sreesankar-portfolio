c40afba962ed314c31bf2fb15d91d9cb089fc4da
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-01-21 13:13:07 +0530

    Fixed issues in sennding mail

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,6 +1,6 @@
 import "./style.css";
 import emailjs from "@emailjs/browser";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -12,6 +12,12 @@ const ContactForm = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [responseMessage, setResponseMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    console.log(import.meta.env.EMAILJS_PUBLIC_KEY);
+    console.log(import.meta.env.EMAILJS_SERVICE_ID);
+    console.log(import.meta.env.EMAILJS_TEMPLATE_ID);
+  }, []);
 
   const validateFullName = (name) => {
     const trimmedName = name.trim(); // Trim leading and trailing spaces
@@ -34,7 +40,7 @@ const ContactForm = () => {
     e.preventDefault();
     // Check if other fields are filled
     if (!formData.email || !formData.message) {
-      setResponseMessage("Please fill out all fields.");
+      setErrorMessage("Please fill out all fields.");
       return;
     }
     // Validate Full Name
@@ -44,33 +50,38 @@ const ContactForm = () => {
 
     setIsSubmitting(true);
     setResponseMessage("");
+    setErrorMessage("");
+
     emailjs
       .send(
-        import.meta.env.EMAILJS_SERVICE_ID,
-        import.meta.env.EMAILJS_TEMPLATE_ID,
+        // import.meta.env.EMAILJS_SERVICE_ID,
+        "service_t57fw04",
+        // import.meta.env.EMAILJS_TEMPLATE_ID,
+        "template_g34nydo",
         {
           fullname: formData.fullname,
           email: formData.email,
           message: formData.message,
         },
-        import.meta.env.EMAILJS_PUBLIC_KEY
+        "SS4xRj8V4AnhdZ-Aw"
       )
       .then((response) => {
         console.log("Success!", response.status, response.text);
         setResponseMessage("Your message has been sent successfully!");
         setFormData({ fullname: "", email: "", message: "" });
         setNameError("");
+        setErrorMessage("");
       })
       .catch((error) => {
         console.error("Failed..", error);
-        setResponseMessage(
-          "Failed to Send your message. Please try again later."
-        );
+        setErrorMessage("Failed to Send your message. Please try again later.");
       })
       .finally(() => setIsSubmitting(false));
   };
   const handleChange = (e) => {
     const { name, value } = e.target;
+    setErrorMessage("");
+    setResponseMessage("");
     setFormData({ ...formData, [name]: value });
   };
 
@@ -121,6 +132,7 @@ const ContactForm = () => {
         {responseMessage && (
           <p className="response-message">{responseMessage}</p>
         )}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
       </form>
     </section>
   );
