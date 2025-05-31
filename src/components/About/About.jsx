ce825445aee43cccb44b6c38140ba4dbe28ff4f0
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-05-31 22:22:40 +0530

    fixed bug in About Section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,16 @@
 import "./style.css";
-import profilePic from "../../assets/sree.jpg"
+import profilePic from "../../assets/sree.jpg";
 import Skills from "../../constants/Skills.jsx";
 const About = () => {
   return (
     <section className="about" id="About">
       <h2 className="about__header">About me</h2>
       <div className="about__content">
-        <img src={profilePic} alt="profile" className="about__image" />
+        <img
+          src="/assets/sree.jpg"
+          alt="Sree Sankar"
+          className="about__image"
+        />
         <p className="about__description">
           {`Hi, I’m Sreesankar P, A passionate web developer specializing in creating dynamic and responsive web applications. With a strong foundation in React and the MERN stack, I enjoy turning ideas into user-friendly digital solutions. Let’s build something amazing together!`}
         </p>
